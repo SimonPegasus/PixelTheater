e4cd737cc7a0bf8bd82286299b15cc9ce1df05da
Simon Pegasus <104474631+SimonPegasus@users.noreply.github.com>
2022-07-08 17:56:09 -0400

    Movement added

--- Pixel Theater/client/src/client.js
+++ Pixel Theater/client/src/client.js
@@ -1,54 +1,95 @@
-// Server stuff
-const sock = io();
-
-
 // Handle serversided events here
-sock.emit('initCL', 'datahere'); // On first join
+const sock = io();
+var id = null;
+
+sock.emit('initCL'); // On first join
+sock.on('CLid', (d) => { // On each server tick
+  player.id = d;
+});
 
 sock.on('tick', () => { // On each server tick
   draw();
+  window.scrollTo(player.x-innerWidth/2, player.y-innerHeight/2); // Keep player in center of screen
+
+  if (document.body === document.activeElement) {
+    if (keys['SHIFT']) {player.speed = 3} else {player.speed = 2}; // Sprint
+    if (keys['S']) { // Down
+      sock.emit('requestMovement', [player.id, 3, player.speed]);
+    };
+    if (keys['W']) { // Up
+      sock.emit('requestMovement', [player.id, 1, player.speed]);
+    };
+    if (keys['D']) { // Right
+      sock.emit('requestMovement', [player.id, 4, player.speed]);
+    };
+    if (keys['A']) { // Left
+      sock.emit('requestMovement', [player.id, 2, player.speed]);
+    };
+  };
+});
+
+sock.on('receiveMovement', (d) => { // On player movement
+  if (player.x-player.w <= 0) {player.x = player.w}; // Out of bounds
+  if (player.y-player.h <= 0) {player.y = player.h}; // Out of bounds
+  if (player.x+player.w >= canvas.width) {player.x = canvas.width-player.w}; // Out of bounds
+  if (player.y+player.h >= canvas.height) {player.y = canvas.height-player.h}; // Out of bounds
+
+  if (d[0] == player.id) {
+    if (d[1] == 3) {
+      player.y += d[2];
+    } else if (d[1] == 1) {
+      player.y -= d[2];
+    } else if (d[1] == 4) {
+      player.x += d[2];
+    } else if (d[1] == 2) {
+      player.x -= d[2];
+    }
+  } else {
+    return;
+  };
 });
 
 
 // Handle clientsided events here / Main
 var c = null;
-
+const canv = document.querySelector('canvas');
+canv.width = 5000, canv.height = 5000;
 var keys = [];
 
 
 
-window.onload = function() {
-  c = document.getElementById('canvas').getContext('2d');
-  draw();
+window.onload = function() { // Once the website properly loads
+  c = canv.getContext('2d');
   c.font = "bold 8pt sans-serif";
 };
 
-function draw() {
-  if (c == null) { return; }
+function draw() { // Render game (clear last frame, draw new one)
+  if (c == null) { return; } // If the website hasn't properly loaded yet, get out of this procedure
   
+  c.clearRect(0,0,canv.width,canv.height);
   player.draw();
 };
 
 
 
 class Player { // Client player
-  constructor(x, y) {
+  constructor(id, x, y) { // Called upon initializing player
     this.w = 16;
     this.h = 16;
     this.x = x;
     this.y = y;
+    this.id = id;
+    this.speed = 2;
   };
-
-  draw() {
+  draw() { // Render player
     c.beginPath();
-    c.fillStyle = "#ff0000";
-    c.fillRect(this.x-8, this.y-8, this.x+8, this.y+8);
-    c.fillRect(10,10,14,14);
+    c.fillStyle = "blue";
+    c.arc(this.x, this.y, this.w, 0, 2*Math.PI);
+    c.fill()
     c.closePath();
   }
 };
-
-const player = new Player(32, 32);
+const player = new Player(id, 16, 16); // Create a new instance of the player
 
 
 // Keystrokes
