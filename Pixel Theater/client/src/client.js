8191e253b716298f604b6658f70e8f66a7528ef2
Simon Pegasus <104474631+SimonPegasus@users.noreply.github.com>
2022-07-08 01:18:30 -0400

    Add files via upload

--- Pixel Theater/client/src/client.js
+++ Pixel Theater/client/src/client.js
@@ -2,9 +2,59 @@
 const sock = io();
 
 
-
-sock.emit('init', 'datahere'); // On first join
+// Handle serversided events here
+sock.emit('initCL', 'datahere'); // On first join
 
 sock.on('tick', () => { // On each server tick
-  console.log('testing');
+  draw();
+});
+
+
+// Handle clientsided events here / Main
+var c = null;
+
+var keys = [];
+
+
+
+window.onload = function() {
+  c = document.getElementById('canvas').getContext('2d');
+  draw();
+  c.font = "bold 8pt sans-serif";
+};
+
+function draw() {
+  if (c == null) { return; }
+  
+  player.draw();
+};
+
+
+
+class Player { // Client player
+  constructor(x, y) {
+    this.w = 16;
+    this.h = 16;
+    this.x = x;
+    this.y = y;
+  };
+
+  draw() {
+    c.beginPath();
+    c.fillStyle = "#ff0000";
+    c.fillRect(this.x-8, this.y-8, this.x+8, this.y+8);
+    c.fillRect(10,10,14,14);
+    c.closePath();
+  }
+};
+
+const player = new Player(32, 32);
+
+
+// Keystrokes
+addEventListener('keydown', function (e) { // Keys down
+  keys[e.key.toUpperCase()] = true;
+});
+addEventListener('keyup', function (e) { // Keys up
+  keys[e.key.toUpperCase()] = false;
 });
