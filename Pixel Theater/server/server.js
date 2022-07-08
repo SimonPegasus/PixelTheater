288ef11944a37de21b3c5622276d116c542fd0c6
Simon Pegasus <104474631+SimonPegasus@users.noreply.github.com>
2022-07-08 01:16:16 -0400

    Add files via upload

--- Pixel Theater/server/server.js
+++ Pixel Theater/server/server.js
@@ -1,11 +1,11 @@
-const port = 80; // Server port
+const port = 80; // Server port (default 80)
 const version = 'Pixel Theater 0.0.0'; // Version
 
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const colors = require('colors');
- 
+
 const app = express();
 
 app.use(express.static(`${__dirname}/../client`));
@@ -19,18 +19,18 @@ server.listen(port, () => { // On start
   console.log('['+colors.gray(date.getHours()+':'+date.getMinutes()+':'+date.getSeconds())+'] '+colors.green.bold('Listening on port '+port));
 });
 
-
-
-io.on('connection', (sock) => { // Called on each client connection
-  sock.on('init', (data) => {
-    console.log(data);
-  });
-});
-
 server.on('error', (err) => { // On error
   console.error(err);
 });
 
-setInterval(() => { // Send ticks to clients
+
+
+io.on('connection', (sock) => { // Called on each client connection
+  sock.on('initCL', (data) => {
+    console.log(data); // Placeholder, supposed to do stuff with the data and send data back to all clients such as location data, etc.
+  });
+});
+
+setInterval(() => { // Send ticks to clients, 20 TPS
   io.emit('tick');
 }, 50);
