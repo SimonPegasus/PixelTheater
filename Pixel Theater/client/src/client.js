eb106478dfded5836459794f8af3809821e683f3
Simon Pegasus <104474631+SimonPegasus@users.noreply.github.com>
2022-07-09 19:49:03 -0400

    Update client.js

--- Pixel Theater/client/src/client.js
+++ Pixel Theater/client/src/client.js
@@ -1,54 +1,32 @@
 // Handle serversided events here
 const sock = io();
 var id = null;
+var players = [];
 
 sock.emit('initCL'); // On first join
 sock.on('CLid', (d) => { // On each server tick
-  player.id = d;
+  player.id = d[0];
+  players = d[1];
 });
 
 sock.on('tick', () => { // On each server tick
-  draw();
-  window.scrollTo(player.x-innerWidth/2, player.y-innerHeight/2); // Keep player in center of screen
-
-  if (document.body === document.activeElement) {
-    if (keys['SHIFT']) {player.speed = 3} else {player.speed = 2}; // Sprint
-    if (keys['S']) { // Down
-      sock.emit('requestMovement', [player.id, 3, player.speed]);
-    };
-    if (keys['W']) { // Up
-      sock.emit('requestMovement', [player.id, 1, player.speed]);
-    };
-    if (keys['D']) { // Right
-      sock.emit('requestMovement', [player.id, 4, player.speed]);
-    };
-    if (keys['A']) { // Left
-      sock.emit('requestMovement', [player.id, 2, player.speed]);
-    };
-  };
+  update();
 });
 
 sock.on('receiveMovement', (d) => { // On player movement
-  if (player.x-player.w <= 0) {player.x = player.w}; // Out of bounds
-  if (player.y-player.h <= 0) {player.y = player.h}; // Out of bounds
-  if (player.x+player.w >= canv.width) {player.x = canv.width-player.w}; // Out of bounds
-  if (player.y+player.h >= canv.height) {player.y = canv.height-player.h}; // Out of bounds
-
-  if (d[0] == player.id) {
-    if (d[1] == 3) { // Down
-      player.y += d[2];
-    } else if (d[1] == 1) { // Up
-      player.y -= d[2];
-    } else if (d[1] == 4) { // Right
-      player.x += d[2];
-    } else if (d[1] == 2) { // Left
-      player.x -= d[2];
-    }
-  } else {
-    return;
-  };
+  players[players.findIndex(player => player.id==d[0])].x = d[1];
+  players[players.findIndex(player => player.id==d[0])].y = d[2];
 });
 
+sock.on('playerconnect', (d) => { // Update player list when a new player connects
+  if (d[1] != player.id) {
+    players.push(d[0]);
+  };
+});
+sock.on('playerdisconnect', (d) => { // Update player list when a player disconnects
+  var i = players.findIndex(player => player.id==d);
+  if (i !== -1) {players.splice(i, 1);}
+});
 
 // Handle clientsided events here / Main
 var c = null;
@@ -63,23 +41,25 @@ window.onload = function() { // Once the website properly loads
   c.font = "bold 8pt sans-serif";
 };
 
-function draw() { // Render game (clear last frame, draw new one)
+function update() { // Render game (clear last frame, draw new one)
   if (c == null) { return; } // If the website hasn't properly loaded yet, get out of this procedure
   
-  c.clearRect(player.x-((screen.width/2)-32),player.y-((screen.height/2)-32),player.x+((screen.width/2)+32),player.y+((screen.height/2)+32)); // Clear last frame
+  //c.clearRect(player.x-((screen.width/2)-32),player.y-((screen.height/2)-32),player.x+((screen.width/2)+32),player.y+((screen.height/2)+32)); // Clear last frame
+  c.clearRect(0,0,canv.width,canv.height);
+  window.scrollTo(player.x-innerWidth/2, player.y-innerHeight/2); // Keep player in center of screen
+
   player.draw();
+  player.update();
 };
 
 
 
 class Player { // Client player
   constructor(id, x, y) { // Called upon initializing player
-    this.w = 16;
-    this.h = 16;
-    this.x = x;
-    this.y = y;
+    this.w = 16, this.h = 16;
+    this.x = x, this.y = y;
     this.id = id;
-    this.speed = 2;
+    this.vel = 2;
   };
   draw() { // Render player
     c.beginPath();
@@ -87,7 +67,32 @@ class Player { // Client player
     c.arc(this.x, this.y, this.w, 0, 2*Math.PI);
     c.fill()
     c.closePath();
-  }
+  };
+  update() { // Check for input and update player
+    if (document.body === document.activeElement) {
+      if (keys['SHIFT']) {this.vel = 3} else {this.vel = 2}; // Sprint
+      if (keys['S']) { // Down
+        this.y += this.vel;
+        sock.emit('applyMovement', [this.id, this.x, this.y]);
+      };
+      if (keys['W']) { // Up
+        this.y -= this.vel;
+        sock.emit('applyMovement', [this.id, this.x, this.y]);
+      };
+      if (keys['D']) { // Right
+        this.x += this.vel;
+        sock.emit('applyMovement', [this.id, this.x, this.y]);
+      };
+      if (keys['A']) { // Left
+        this.x -= this.vel;
+        sock.emit('applyMovement', [this.id, this.x, this.y]);
+      };
+    };
+    if (player.x-player.w <= 0) {player.x = player.w}; // Out of bounds
+    if (player.y-player.h <= 0) {player.y = player.h}; // Out of bounds
+    if (player.x+player.w >= canv.width) {player.x = canv.width-player.w}; // Out of bounds
+    if (player.y+player.h >= canv.height) {player.y = canv.height-player.h}; // Out of bounds
+  };
 };
 const player = new Player(id, 16, 16); // Create a new instance of the player
 
